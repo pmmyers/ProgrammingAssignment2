4e9ceb5d0592186ded98be7c73f0939c4d503ff8
sit-dog <pm5395a@gmail.com>
2021-12-22 19:22:09 -0500

    Working on the problem to start

--- cachematrix.R
+++ cachematrix.R
@@ -3,8 +3,22 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+makeCacheMatrix <- function(myMatrix = matrix()) {
+    inv.myMatrix <- NULL
+    set <- function(s.myMatrix) {
+        myMatrix <<- s.myMatrix
+        inv.myMatrix <<- NULL
+    }
+    get <- function() myMatrix
+    setInverse <- function(football) inv.myMatrix <<- football
+    getInverse <- function() inv.myMatrix
+    list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
+    
 
+    
+
+    
+        
 }
 
 
@@ -13,3 +27,10 @@ makeCacheMatrix <- function(x = matrix()) {
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
 }
+
+
+
+
+##Sandbox
+
+myMatrix <- matrix(c(6,2,8,4), nrow = 2, ncol = 2)
